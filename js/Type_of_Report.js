9d7182c056dc82d78353a64c1f461a365a3be76b
ford-from-the-future <khalic44@gmail.com>
2022-11-17 14:43:18 -0500

    big boi hours

--- js/Type_of_Report.js
+++ js/Type_of_Report.js
@@ -28,33 +28,7 @@ Type_of_Report.call = () => {
                 callback: function (result) {
                     //--------------------
 
-                    $(() => {
-                        // Handle Login Button Click event
-                        $('WEEKLY INVOICE OF MEMBER ACCOUNT').on('click', function(e) {
-                            // If the login is successful set the current user to the logged in user then store the database in localStorage
-                            if(login($('#username').val(), $('#password').val()))
-                            {
-                                db.currentUser = login($('#username').val(), $('#password').val());
-                                window.localStorage.setItem("db", JSON.stringify(db));
-                                // Check the users type and send them to the appropriate dashboard.
-                                switch(db.currentUser.type)
-                                {
-                                    case 0:
-                                        //window.open('member_dashboard.html','_self',false)
-                                        member_dashboard.call();
-                                        break;
-                                    case 1:
-                                        manager_dashboard.call();
-                                        break;
-                                    case 2:
-                                        break
-                                    default:
-                                        bootbox.alert("Something went wrong.");
-                                }
-                            }
 
-                        });
-                    });
 
                     //----------------
                 },
